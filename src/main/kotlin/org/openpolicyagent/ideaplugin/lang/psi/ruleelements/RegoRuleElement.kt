19953602a9f043bd17729bdc5c1d48cdf91046e5
Andrea Canonica <andrea.canonica@inventage.com>
2023-04-03 14:03:24 +0200

    Implement References - wip (#113)

---
+++ src/main/kotlin/org/openpolicyagent/ideaplugin/lang/psi/ruleelements/RegoRuleElement.kt
@@ -0,0 +1,10 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.openpolicyagent.ideaplugin.lang.psi.ruleelements
+
+import com.intellij.psi.PsiNameIdentifierOwner
+
+interface RegoRuleElement : PsiNameIdentifierOwner
